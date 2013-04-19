87c7332f1908e18bd3a98dcc44afe2751af0c648
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-19 16:29:11 +0200

    / ‘src/unitTests/testfacefeaturesdetection.h’

--- src/unitTests/testfacefeaturesdetection.h
+++ src/unitTests/testfacefeaturesdetection.h
@@ -33,7 +33,7 @@ public:
     {
         QVector<cv::flann::IndexParams *> params;
         Mesh face = Mesh::fromOBJ(pathToOBJ);
-        cv::flann::KDTreeIndexParams *p = new cv::flann::KDTreeIndexParams();
+        cv::flann::IndexParams *p = new cv::flann::CompositeIndexParams();
         params << p;
         SurfaceProcessor::smooth(face, 20, 1.0, 1, *(params.at(0)) );
 
