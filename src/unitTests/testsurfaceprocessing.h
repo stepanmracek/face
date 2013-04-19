cf93425d1223140d3539b276b96c63a2866b459f
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-19 12:37:47 +0200

    / ‘src/unitTests/testsurfaceprocessing.h’

--- src/unitTests/testsurfaceprocessing.h
+++ src/unitTests/testsurfaceprocessing.h
@@ -9,8 +9,8 @@ class TestSurfaceProcessing
 public:
     static void testNormals()
     {
-        Mesh m = Mesh::fromOBJ("/home/stepo/face.obj");
-        SurfaceProcessor::smooth(m, 0.5, 20);
+        Mesh m = Mesh::fromOBJ("/home/stepo/face2.obj");
+        //SurfaceProcessor::smooth(m, 0.5, 20);
         SurfaceProcessor::calculateNormals(m, 20);
 
         MapConverter converter;
