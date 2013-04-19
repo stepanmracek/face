a16b119a4c880cdcdfc20f825e124d6681988da2
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-19 14:23:07 +0200

    / ‘src/unitTests/testfacefeaturesdetection.h’

--- src/unitTests/testfacefeaturesdetection.h
+++ src/unitTests/testfacefeaturesdetection.h
@@ -36,7 +36,7 @@ public:
 
         MapConverter converter;
         Map depthMap = SurfaceProcessor::depthmap(face, converter, 1, ZCoord);
-        SurfaceProcessor::smooth(depthMap, 0.5, 20);
+        SurfaceProcessor::smooth(depthMap, 1, 10);
 
         Matrix depthImage = depthMap.toMatrix();
         cv::imshow("depthMap", depthImage);
