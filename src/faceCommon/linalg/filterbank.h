bfc1c91f158ece1b33e871a03d1fd2aa0c53c619
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-24 09:55:50 +0200

    / ‘src/faceCommon/linalg/filterbank.h’

--- src/faceCommon/linalg/filterbank.h
+++ src/faceCommon/linalg/filterbank.h
@@ -1,4 +1,25 @@
 #ifndef FILTERBANK_H
 #define FILTERBANK_H
 
+#include <QVector>
+
+#include "common.h"
+#include "facelib/map.h"
+
+class FilterBank
+{
+public:
+    QVector<Matrix> realKernels;
+    QVector<Matrix> imagKernels;
+
+    QVector<Map> getRealResponse(Map &map);
+    QVector<Matrix> getRealResponse(Matrix &mat);
+
+    QVector<Map> getImagResponse(Map &map);
+    QVector<Matrix> getImagResponse(Matrix &mat);
+
+    QVector<Map> getAbsResponse(Map &map);
+    QVector<Matrix> getAbsResponse(Matrix &mat);
+};
+
 #endif // FILTERBANK_H
