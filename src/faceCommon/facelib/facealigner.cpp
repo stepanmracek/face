1d733facc72b6597b006730529d65226ae34882d
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-24 18:33:55 +0200

    / ‘src/faceCommon/facelib/facealigner.cpp’

--- src/faceCommon/facelib/facealigner.cpp
+++ src/faceCommon/facelib/facealigner.cpp
@@ -12,5 +12,15 @@ void FaceAligner::align(Mesh &face)
     Landmarks lm = lmDetector.Detect();
     face.move(-lm.get(Landmarks::Nosetip));
 
+    MapConverter converter;
+    Map depth = SurfaceProcessor::depthmap(face, converter, 1.0, ZCoord);
+
+    for (int y = -10; y <= 20; y += 5)
+    {
+        for (x = -10; x <= 10; x += 5)
+        {
+            cv::Point2d mapPoint = converter.MeshToMapCoords(depth, cv::Point2d(x, y));
+        }
+    }
 
 }
