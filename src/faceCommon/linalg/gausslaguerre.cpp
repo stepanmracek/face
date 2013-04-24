6e07e5cb8d527aac25c3e718183625eff4f2185b
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-24 12:18:29 +0200

    / ‘src/faceCommon/linalg/gausslaguerre.cpp’ / ‘src/faceCommon/linalg/gausslaguerre.h’

--- src/faceCommon/linalg/gausslaguerre.cpp
+++ src/faceCommon/linalg/gausslaguerre.cpp
@@ -2,8 +2,35 @@
 
 GaussLaguerre::GaussLaguerre(int size)
 {
-}
+    for (int j = 2; j <= 2; j++)
+    {
+        for (int n = 1; n <= 5; n++)
+        {
+            for (int k = 0; k <= 4; k++)
+            {
+                Matrix re = Matrix::zeros(size, size);
+                Matrix im = Matrix::zeros(size, size);
 
+                for (int y = 0; y < size; y++)
+                {
+                    double realY = size/2 - y;
+                    for (int x = 0; x < size; x++)
+                    {
+                        double realX = size/2 - x;
+                        double r = sqrt(realX*realX + realY*realY);
+                        double theta = atan2(realY, realX);
+
+                        re(y, x) = h(r, theta, n, k, j) * cos(n * theta);
+                        im(y, x) = h(r, theta, n, k, j) * sin(n * theta);
+                    }
+                }
+
+                realKernels << re;
+                imagKernels << re;
+            }
+        }
+    }
+}
 
 unsigned int factorial(unsigned int n)
 {
@@ -25,4 +52,11 @@ double GaussLaguerre::L(double r, int n, int k)
     {
         sum += pow(-1, K) * over(n + k, k - h) * pow(r, h) / factorial(h);
     }
+}
+
+double GaussLaguerre::h(double r, double theta, int n, int k, int j)
+{
+    return pow(-1, k) * pow(2, (fabs(n)+1.0)/2.0) * pow(M_PI, fabs(n)/2.0) *
+            pow((((double)(factorial(k)))/factorial(fabs(n) + k)), 0.5) * pow(r, fabs(n)) *
+            L(2 * M_PI * r * r, n, k) * exp(-M_PI * r * r); // * exp(j * n * theta);
 }
--- src/faceCommon/linalg/gausslaguerre.h
+++ src/faceCommon/linalg/gausslaguerre.h
@@ -1,10 +1,16 @@
 #ifndef GAUSSLAGUERRE_H
 #define GAUSSLAGUERRE_H
 
-class GaussLaguerre
+#include "filterbank.h"
+
+class GaussLaguerre : public FilterBank
 {
 public:
-    GaussLaguerre();
+    GaussLaguerre(int size);
+
+private:
+    double L(double r, int n, int k);
+    double h(double r, double theta, int n, int k, int j);
 };
 
 #endif // GAUSSLAGUERRE_H
