bc9d40bb509fc6d6d2264979e160327bb0f77beb
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-24 12:18:46 +0200

    / ‘src/faceCommon/linalg/gausslaguerre.cpp’

--- src/faceCommon/linalg/gausslaguerre.cpp
+++ src/faceCommon/linalg/gausslaguerre.cpp
@@ -50,7 +50,7 @@ double GaussLaguerre::L(double r, int n, int k)
     double sum = 0;
     for (int h = 0; h <= k; h++)
     {
-        sum += pow(-1, K) * over(n + k, k - h) * pow(r, h) / factorial(h);
+        sum += pow(-1, k) * over(n + k, k - h) * pow(r, h) / factorial(h);
     }
 }
 
