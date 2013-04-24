4881e6122b312319dcae53f6aeaff7b98518ee15
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-24 18:23:53 +0200

    / ‘src/faceCommon/facelib/facealigner.cpp’ / ‘src/faceCommon/facelib/facealigner.h’

--- src/faceCommon/facelib/facealigner.cpp
+++ src/faceCommon/facelib/facealigner.cpp
@@ -1,5 +1,16 @@
 #include "facealigner.h"
 
+#include "facelib/landmarkdetector.h"
+
 FaceAligner::FaceAligner()
 {
+}
+
+void FaceAligner::align(Mesh &face)
+{
+    LandmarkDetector lmDetector(face);
+    Landmarks lm = lmDetector.Detect();
+    face.move(-lm.get(Landmarks::Nosetip));
+
+
 }
--- src/faceCommon/facelib/facealigner.h
+++ src/faceCommon/facelib/facealigner.h
@@ -1,10 +1,14 @@
 #ifndef FACEALIGNER_H
 #define FACEALIGNER_H
 
+#include "facelib/mesh.h"
+
 class FaceAligner
 {
 public:
     FaceAligner();
+
+    void align(Mesh &face);
 };
 
 #endif // FACEALIGNER_H
