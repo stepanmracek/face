9c4448f260ace0d712aa4fb86174a37bd5ac8556
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-24 11:03:15 +0200

    / ‘src/faceCommon/linalg/gausslaguerre.cpp’

--- src/faceCommon/linalg/gausslaguerre.cpp
+++ src/faceCommon/linalg/gausslaguerre.cpp
@@ -1,5 +1,28 @@
 #include "gausslaguerre.h"
 
-GaussLaguerre::GaussLaguerre()
+GaussLaguerre::GaussLaguerre(int size)
 {
+}
+
+
+unsigned int factorial(unsigned int n)
+{
+    unsigned int ret = 1;
+    for(unsigned int i = 1; i <= n; ++i)
+        ret *= i;
+    return ret;
+}
+
+double over(double n, double k)
+{
+    return factorial(n)/(factorial(k) * factorial(n-k));
+}
+
+double GaussLaguerre::L(double r, int n, int k)
+{
+    double sum = 0;
+    for (int h = 0; h <= K; h++)
+    {
+        sum += pow(-1, K) * over(n + k, k - h) * pow(r, h) / factorial(h);
+    }
 }
