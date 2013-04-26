9c67a7e1cf557754d81dd9d7d1c61016fff7d9de
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-26 08:07:01 +0200

    / ‘src/unitTests/testfacefeaturesdetection.h’

--- src/unitTests/testfacefeaturesdetection.h
+++ src/unitTests/testfacefeaturesdetection.h
@@ -253,7 +253,7 @@ public:
         Mesh mean = Mesh::fromOBJ(dirPath + QDir::separator() + "mean.obj");
         FaceAligner aligner(mean);
         Mesh face = Mesh::fromXYZFile(dirPath + QDir::separator() + fileName);
-        face.rotate(cv::Vec3d(0,0,1.0));
+        face.rotate(cv::Vec3d(0,0,0.1));
         Mesh old = face;
         old.move(cv::Point3d(100,0,0));
         //aligner.meanFace.writeOBJ(dirPath + QDir::separator() + "mean.obj", '.');
