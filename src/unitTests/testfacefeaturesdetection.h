07fcf0162b93321c66675de2cd5650923bcdb089
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-26 09:41:19 +0200

    / ‘src/unitTests/testfacefeaturesdetection.h’

--- src/unitTests/testfacefeaturesdetection.h
+++ src/unitTests/testfacefeaturesdetection.h
@@ -256,11 +256,11 @@ public:
         face.rotate(cv::Vec3d(0.15, 0.15, 0.15));
         Mesh old = face;
         //aligner.meanFace.writeOBJ(dirPath + QDir::separator() + "mean.obj", '.');
-        aligner.align(face, 10);
+        aligner.align(face, 3);
 
         old.move(cv::Point3d(100,0,0));
         face.move(cv::Point3d(-100,0,0));
-        aligner.meanFace.move(cv::Point3d(-100,0,20));
+        aligner.meanFace.move(cv::Point3d(-100,0,10));
 
         QApplication app(argc, argv);
         GLWidget widget;
