dd25ba9daeb0d1fbde8bdac09a41323723a2bbfd
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-26 13:04:54 +0200

    / ‘src/unitTests/testmap.h’

--- src/unitTests/testmap.h
+++ src/unitTests/testmap.h
@@ -1,4 +1,22 @@
 #ifndef TESTMAP_H
 #define TESTMAP_H
 
+#include <QString>
+
+#include "facelib/mesh.h"
+#include "facelib/map.h"
+#include "facelib/surfaceprocessor.h"
+
+class TestMap
+{
+public:
+    static void testSerialization(const QString &pathToXYZ)
+    {
+        Mesh mesh = Mesh::fromXYZFile(pathToXYZ);
+        MapConverter c;
+        Map map = SurfaceProcessor::depthmap(mesh, c, 1.0, ZCoord);
+        map.serialize("serializedMap.map");
+    }
+};
+
 #endif // TESTMAP_H
