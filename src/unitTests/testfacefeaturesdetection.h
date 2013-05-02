aeb583fee4f72dfd6ffdc65f3d1aceda9586b992
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-02 12:49:39 +0200

    / ‘src/unitTests/testfacefeaturesdetection.h’

--- src/unitTests/testfacefeaturesdetection.h
+++ src/unitTests/testfacefeaturesdetection.h
@@ -265,7 +265,7 @@ public:
         QApplication app(argc, argv);
         GLWidget widget;
         widget.setWindowTitle("GL Widget");
-        //widget.addFace(&aligner.meanFace);
+        widget.addFace(&aligner.meanFace);
         widget.addFace(&face);
         widget.addFace(&old);
         widget.show();
