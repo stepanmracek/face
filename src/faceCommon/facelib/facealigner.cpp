d3dfc8c6dc5f5e4b93201394f7788f6151a7ccef
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-02 15:40:56 +0200

    / ‘src/faceCommon/facelib/facealigner.cpp’

--- src/faceCommon/facelib/facealigner.cpp
+++ src/faceCommon/facelib/facealigner.cpp
@@ -99,10 +99,10 @@ Landmarks FaceAligner::align(Mesh &face, int iterations)
 
         MapConverter converter;
         Map depth = SurfaceProcessor::depthmap(face, converter, 1.0, ZCoord);
-        cv::imshow("smooth1", depth.toMatrix());
+        //cv::imshow("smooth1", depth.toMatrix());
         depth.applyFilter(smoothKernel, 3, true);
-        cv::imshow("smooth2", depth.toMatrix());
-        cv::waitKey();
+        //cv::imshow("smooth2", depth.toMatrix());
+        //cv::waitKey();
 
         double minTheta;
         double minD = 1e300;
@@ -159,7 +159,7 @@ Landmarks FaceAligner::align(Mesh &face, int iterations)
             }
         }
 
-        qDebug() << minTheta;
+        qDebug() << minTheta << minD;
 
         face.rotate(0, 0, -minTheta);
         face.move(minMove);
