a895fa7ff75be583e0a1686ac9221b8dd4d17803
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-02 11:13:04 +0200

    / ‘src/faceCommon/facelib/facealigner.cpp’

--- src/faceCommon/facelib/facealigner.cpp
+++ src/faceCommon/facelib/facealigner.cpp
@@ -132,7 +132,7 @@ Landmarks FaceAligner::align(Mesh &face, int iterations)
         Map testMap = SurfaceProcessor::depthmap(sampledFace, testMC, 1, ZCoord);
         cv::imshow("test", testMap.toMatrix());
         cv::imshow("testDepth", testDepth);
-        cv::waitKey(30);
+        cv::waitKey(1);
 
         //Matrix rotationCandidate = Procrustes3D::getOptimalRotation(sampledFace.points, referencePoints);
         //Procrustes3D::transform(sampledFace.points, rotationCandidate);
