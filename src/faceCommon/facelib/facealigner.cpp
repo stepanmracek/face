f55e6e1fa621788a1556dd36286151aed3da4d35
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-02 17:10:36 +0200

    / ‘src/faceCommon/facelib/facealigner.cpp’

--- src/faceCommon/facelib/facealigner.cpp
+++ src/faceCommon/facelib/facealigner.cpp
@@ -90,6 +90,7 @@ Landmarks FaceAligner::align(Mesh &face, int maxIterations)
     assert(maxIterations >= 1);
     LandmarkDetector lmDetector(face);
     Landmarks lm = lmDetector.detect();
+    qDebug()<< "FaceAligner::align - nose" << lm.get(Landmarks::Nosetip).x << lm.get(Landmarks::Nosetip).y << lm.get(Landmarks::Nosetip).z;
 
     Matrix smoothKernel = KernelGenerator::gaussianKernel(5);
     double totalMinD = 1e300;
