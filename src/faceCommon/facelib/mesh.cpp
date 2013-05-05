ea017c3600c9167b30b614e09462c77e7d913ac7
Štěpán Mráček <stepan.mracek@gmail.com>
2013-05-05 21:55:53 +0200

    / ‘src/faceCommon/facelib/mesh.cpp’

--- src/faceCommon/facelib/mesh.cpp
+++ src/faceCommon/facelib/mesh.cpp
@@ -370,7 +370,7 @@ Mesh Mesh::fromABS(const QString &filename, const QString &texture, bool central
 
             if (flags[i+1] && flags[i+640])
             {
-                qDebug() << i;
+                qDebug() << i << totalToUsed[flags[i]] << totalToUsed[flags[i+1]] << totalToUsed[flags[i+640]];
                 mesh.triangles << cv::Vec3i(totalToUsed[flags[i]], totalToUsed[flags[i+1]], totalToUsed[flags[i+640]]);
             }
         }
