b187011339e5f27360a7738918f9b124c1b025c8
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-09 16:56:31 +0200

    / ‘src/kinect/kinect.cpp’ / ‘src/kinect/kinect.h’

--- src/kinect/kinect.cpp
+++ src/kinect/kinect.cpp
@@ -84,7 +84,7 @@ bool Kinect::getRGB(uint8_t *rgb)
     return true;
 }
 
-bool Kinect::getRGBIter(uint8_t *rgb)
+bool Kinect::getRGBIter(unsigned char *rgb)
 {
     uint8_t *buffer;
     uint32_t ts;
