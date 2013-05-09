b68d95fb2ceadbd47066ef4c42bf2b2102d03a04
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-09 20:24:09 +0200

    / ‘src/appKinectAcquire/main.cpp’

--- src/appKinectAcquire/main.cpp
+++ src/appKinectAcquire/main.cpp
@@ -45,7 +45,7 @@ int align(int argc, char *argv[])
 
     Mesh meanForAlign = Mesh::fromOBJ("../../test/meanForAlign.obj");
     FaceAligner aligner(meanForAlign);
-    aligner.icpAlign(morphedMesh, 10);
+    aligner.icpAlign(morphedMesh, 9);
 
     QApplication app(argc, argv);
     GLWidget widget;
