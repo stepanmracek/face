9dddd2b198aa2dbbc9574e9ccd3dbdb173d6229d
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-09 19:39:27 +0200

    / ‘src/faceCommon/facelib/morphable3dfacemodel.cpp’

--- src/faceCommon/facelib/morphable3dfacemodel.cpp
+++ src/faceCommon/facelib/morphable3dfacemodel.cpp
@@ -154,9 +154,9 @@ Mesh Morphable3DFaceModel::morph(Mesh &inputMesh, Landmarks &inputLandmarks, int
 {
     Procrustes3DResult procrustesResult = align(inputMesh, landmarks, iterations);
     morphModel(inputMesh);
-    Mesh result(mesh);
-    //Procrustes3D::applyInversedProcrustesResult(inputLandmarks.points, procrustesResult);
-    //Procrustes3D::applyInversedProcrustesResult(inputMesh.points, procrustesResult);
+    Mesh result = mesh;
+    Procrustes3D::applyInversedProcrustesResult(inputLandmarks.points, procrustesResult);
+    Procrustes3D::applyInversedProcrustesResult(inputMesh.points, procrustesResult);
     Procrustes3D::applyInversedProcrustesResult(result.points, procrustesResult);
     result.recalculateMinMax();
     inputMesh.recalculateMinMax();
