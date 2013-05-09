50228109ba9586e44628dcfa4805bab1345f4725
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-09 20:04:48 +0200

    / ‘src/faceCommon/facelib/morphable3dfacemodel.cpp’

--- src/faceCommon/facelib/morphable3dfacemodel.cpp
+++ src/faceCommon/facelib/morphable3dfacemodel.cpp
@@ -155,7 +155,6 @@ Mesh Morphable3DFaceModel::morph(Mesh &inputMesh, Landmarks &inputLandmarks, int
     Procrustes3DResult procrustesResult = align(inputMesh, landmarks, iterations);
     morphModel(inputMesh);
     Mesh result = mesh;
-    qDebug() << "procrustesResult:" << procrustesResult.rotations.count();
     Procrustes3D::applyInversedProcrustesResult(inputLandmarks.points, procrustesResult);
     Procrustes3D::applyInversedProcrustesResult(inputMesh.points, procrustesResult);
     Procrustes3D::applyInversedProcrustesResult(result.points, procrustesResult);
