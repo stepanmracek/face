bfcc24f3a97c98102406ea5c6e6fd5a39c539b1d
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-09 16:31:48 +0200

    / ‘src/appMorphFaceModel/main.cpp’

--- src/appMorphFaceModel/main.cpp
+++ src/appMorphFaceModel/main.cpp
@@ -6,11 +6,13 @@
 
 int main(int argc, char *argv[])
 {
-    QString pca = "../../test/align-pca.xml";
-    QString flags = "../../test/align-flags";
-    QString landmarksPath = "../../test/align-landmarks.xml";
+    QString pca = "../../test/morph-pca.xml";
+    QString pcaZcoord = "../../test/morph-pca-zcoord.xml";
+    QString pcaTexture = "../../test/morph-pca-texture.xml";
+    QString flags = "../../test/morph-flags";
+    QString landmarksPath = "../../test/morph-landmarks.xml";
 
-    Morphable3DFaceModel model(pca, flags, landmarksPath, 200);
+    Morphable3DFaceModel model(pcaZcoord, pcaTexture, pca, flags, landmarksPath, 200);
 
     QApplication app(argc, argv);
     Morphable3DFaceModelWidget widget;
