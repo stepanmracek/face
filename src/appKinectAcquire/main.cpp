a0da412f2b09602780559b803d7e7e13923975fa
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-09 20:12:11 +0200

    / ‘src/appKinectAcquire/main.cpp’

--- src/appKinectAcquire/main.cpp
+++ src/appKinectAcquire/main.cpp
@@ -49,7 +49,7 @@ int align(int argc, char *argv[])
 
     QApplication app(argc, argv);
     GLWidget widget;
-    widget.addFace(&inputMesh);
+    widget.addFace(&morphedMesh);
     widget.show();
 
     return app.exec();
