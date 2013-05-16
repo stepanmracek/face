931b449e6bc5be425d1a147a652ef65278913b0d
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-16 15:24:38 +0200

    / ‘src/appEvaluation/evaluate3dfrgc.h’

--- src/appEvaluation/evaluate3dfrgc.h
+++ src/appEvaluation/evaluate3dfrgc.h
@@ -80,10 +80,12 @@ public:
             Matrix gaussKernel = KernelGenerator::gaussianKernel(7);
             depth.applyFilter(gaussKernel, 3, true);
 
-            for (int d = 10; d <= 200; d += 10)
+            int startD = 10;
+            for (int d = startD; d <= 100; d += 10)
             {
                 VectorOfPoints isoCurve = SurfaceProcessor::isoGeodeticCurve(depth, converter, cv::Point3d(0,0,0), d, 100, 2);
                 w.addCurve(isoCurve);
+                Common::savePlot(isoCurve, "isoCurves", d == startD);
             }
 
             w.addFace(&mesh);
