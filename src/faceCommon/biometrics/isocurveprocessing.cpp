20424a76ea475a907496f75031975af87d577415
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-17 09:30:56 +0200

    / ‘src/faceCommon/biometrics/isocurveprocessing.cpp’

--- src/faceCommon/biometrics/isocurveprocessing.cpp
+++ src/faceCommon/biometrics/isocurveprocessing.cpp
@@ -27,3 +27,38 @@ QVector<SubjectIsoCurves> IsoCurveProcessing::readDirectory(const QString &path,
 
     return result;
 }
+
+void IsoCurveProcessing::sampleIsoCurves(QVector<SubjectIsoCurves> &data, int modulo)
+{
+    for (int i = 0; i < data.count(); i++)
+    {
+        SubjectIsoCurves &subj = data[i];
+        VectorOfIsocurves newIsoCurves;
+        for (int j = 0; j < subj.vectorOfIsocurves.count(); j += modulo)
+        {
+            newIsoCurves << subj.vectorOfIsocurves[j];
+        }
+        subj.vectorOfIsocurves = newIsoCurves;
+    }
+}
+
+
+void IsoCurveProcessing::sampleIsoCurvePoints(QVector<SubjectIsoCurves> &data, int modulo)
+{
+    for (int i = 0; i < data.count(); i++)
+    {
+        SubjectIsoCurves &subj = data[i];
+        VectorOfIsocurves newIsoCurves;
+        for (int j = 0; j < subj.vectorOfIsocurves.count(); j++)
+        {
+            VectorOfPoints &isocurve = subj.vectorOfIsocurves[j];
+            VectorOfPoints newIsoCurve;
+            for (int k = 0; k < isocurve.count(); k += modulo)
+            {
+                newIsoCurve << isocurve[k];
+            }
+            newIsoCurves << newIsoCurve;
+        }
+        subj.vectorOfIsocurves = newIsoCurves;
+    }
+}
