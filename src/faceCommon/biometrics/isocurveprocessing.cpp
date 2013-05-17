a301eb46bc79e78eb6e1013403b180cd05b2d2e4
Štěpán Mráček <stepan.mracek@gmai.com>
2013-05-17 09:08:15 +0200

    / ‘src/faceCommon/biometrics/isocurveprocessing.cpp’

--- src/faceCommon/biometrics/isocurveprocessing.cpp
+++ src/faceCommon/biometrics/isocurveprocessing.cpp
@@ -1,5 +1,29 @@
 #include "isocurveprocessing.h"
 
+#include <QDir>
+#include <QFileInfoList>
+
+#include "linalg/serialization.h"
+
 IsoCurveProcessing::IsoCurveProcessing()
 {
+}
+
+QVector<SubjectIsoCurves> IsoCurveProcessing::readDirectory(const QString &path, const QString &separator, const QString &fileNameFilter)
+{
+    QDir dir(path, fileNameFilter);
+    QFileInfoList files = dir.entryInfoList();
+
+    QVector<SubjectIsoCurves> result;
+    foreach(const QFileInfo &fileInfo, files)
+    {
+        SubjectIsoCurves subject;
+
+        subject.id = fileInfo.baseName().split(separator)[0];
+        subject.vectorOfIsocurves = Serialization::readVectorOfPointclouds(fileInfo.absoluteFilePath());
+
+        result << subject;
+    }
+
+    return result;
 }
