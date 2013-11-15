5d80d4313e8199f2b71a2149caa1a7250c21785d
Štěpán Mráček <stepan.mracek@gmail.com>
2013-11-15 14:23:04 +0100

    appKinect cleanup

--- src/appKinectAcquire/main.cpp
+++ src/appKinectAcquire/main.cpp
@@ -9,87 +9,8 @@
 #include "facelib/facealigner.h"
 #include "facelib/surfaceprocessor.h"
 #include "dlgscanface.h"
-
 #include "frmkinectmain.h"
-
 #include "kinectsensorplugin.h"
-
-int scan(int argc, char *argv[])
-{
-    Mesh *m = Kinect::scanFace(10, "../../test/haar-face.xml");
-    Mesh mean = Mesh::fromOBJ("../../test/meanForAlign.obj");
-    FaceAligner aligner(mean);
-    aligner.icpAlign(*m, 10, FaceAligner::NoseTipDetection);
-
-    MapConverter c;
-    cv::imshow("scan", SurfaceProcessor::depthmap(*m, c, cv::Point2d(-75,-75), cv::Point2d(75,75), 2, Texture_I).toMatrix());
-
-    QApplication app(argc, argv);
-
-    GLWidget widget;
-    //QString scanName = QInputDialog::getText(&widget, "Scan name", "Scan name:", QLineEdit::Normal, "");
-    //widget.setWindowTitle(scanName);
-    //m.writeBIN("../../test/kinect/" + scanName + ".bin");
-
-    widget.addFace(m);
-    widget.show();
-
-    return app.exec();
-}
-
-int scan(int argc, char *argv[], const QString &outputPath, const QString &lmPath)
-{
-    Mesh *m = Kinect::scanFace(10, "../../test/haar-face.xml");
-    bool success;
-    Landmarks lm = FaceFeaturesAnotation::anotate(*m, success);
-    if (!success)
-    {
-        return 0;
-    }
-
-    QApplication app(argc, argv);
-    GLWidget widget;
-    widget.addFace(m);
-    widget.addLandmarks(&lm);
-    widget.show();
-
-    m->writeBIN(outputPath);
-    lm.serialize(lmPath);
-
-    return app.exec();
-}
-
-int align(int argc, char *argv[])
-{
-    Mesh *inputMesh = Kinect::scanFace(10, "../../test/haar-face.xml"); // Mesh::fromBIN("../../test/kinect-face.bin", false);
-    //Landmarks landmarks("../../test/kinect-face.xml");
-
-    QString pca = "../../test/morph-pca.xml";
-    QString pcaZcoord = "../../test/morph-pca-zcoord.xml";
-    QString pcaTexture = "../../test/morph-pca-texture.xml";
-    QString flags = "../../test/morph-flags";
-    QString landmarksPath = "../../test/morph-landmarks.xml";
-    Morphable3DFaceModel model(pcaZcoord, pcaTexture, pca, flags, landmarksPath, 200);
-
-    Mesh morphedMesh = model.morph(*inputMesh, 10); //landmarks, 10);
-
-    QApplication app(argc, argv);
-
-    GLWidget widget;
-    QString scanName = QInputDialog::getText(&widget, "Scan name", "Scan name:", QLineEdit::Normal, "");
-    widget.setWindowTitle(scanName);
-    morphedMesh.writeBIN("../../test/kinect/" + scanName + ".bin");
-
-    //morphedMesh.translate(cv::Point3d(-50,0,0));
-    //morphedMesh.colors.clear();
-    widget.addFace(&morphedMesh);
-
-    //inputMesh.translate(cv::Point3d(50,0,0));
-    //widget.addFace(&inputMesh);
-    widget.show();
-
-    return app.exec();
-}
 
 struct Arguments
 {
